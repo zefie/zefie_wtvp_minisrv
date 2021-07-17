e27d283d66485c75b20dd6c714789b3bf4654090
zefie <zefie@zefie.net>
2021-07-17 17:14:12 -0400

    begin initial work on wtv-flashrom (not yet ready)

--- hacktv_updsrv/ServiceVault/wtv-flashrom/lc2-download-complete.js
+++ hacktv_updsrv/ServiceVault/wtv-flashrom/lc2-download-complete.js
@@ -1,35 +1,44 @@
-// todo, actual file logic
-// - ready query param to get flashrom path, check for its existance
-// - handle last part to redirect to lc2-download-complete
-// - handle failures
-
 headers = `200 OK
 Content-type: text/html`
 
 data = `<html>
+
 <head>
 <title>
 Updating
 </title>
-<display switchtowebmode transition=none nostatus nooptions skipback clearback>
+<display switchtowebmode nostatus nooptions skipback clearback>
+
+<meta http-equiv=Refresh content="270; url=client:poweroff?invalRAMImage">
+
 </head>
-<body noscroll bgcolor="#191919" text="#42CC55" link="36d5ff"
+
+<body onLoad="document.forms.invalram.submit()";
+noscroll bgcolor="#191919" text="#42CC55" link="36d5ff"
 hspace=0 vspace=0 fontsize="large">
+
+<form name ="invalram" action="client:invalramimage">
+</form>
+
 <table cellspacing=0 cellpadding=0>
 <tr>
 <td width=104 height=74 valign=middle align=center bgcolor="3B3A4D">
+<img src="wtv-flashrom:/ROMCache/MSNLogo.gif" width=87 height=67>
 <td width=20 valign=top align=left bgcolor="3B3A4D">
+<img src="wtv-flashrom:/ROMCache/Spacer.gif" width=1 height=1>
 <td colspan=10 width=436 valign=middle align=left bgcolor="3B3A4D">
 <font color="D6DFD0" size="+2">
 <blackface>
 <shadow>
+<img src="wtv-flashrom:/ROMCache/Spacer.gif" width=1 height=4>
 <br>
-Updating now...
+Updating complete
 </shadow>
 </blackface>
 </font>
 <tr>
 <td colspan=12 width=560 height=10 valign=top align=left>
+<img src="wtv-flashrom:/ROMCache/S40H1.gif" width=560 height=6>
 <tr>
 <td width=104 height=10 valign=top align=left>
 <td width=20 valign=top align=left>
@@ -43,44 +52,20 @@ Updating now...
 <td width=20 valign=top align=left>
 <td width=68 valign=top align=left>
 <td width=20 valign=top align=left>
-<form action="client:poweroff">
 <tr>
 <td width=104 valign=middle align=center>
 <td width=20 valign=middle align=center>
 <td colspan=9 width=100 height=258 valign=top align=left>
 <font size=+1>
-Your Internet Receiver is being<br>updated automatically.
-<p> <font size=+1>
-This will take a while, and then<br> your unit will reboot.<br><br>
-`;
-
-if (flashrom_isboot && flashrom_part == 16) {
-data += `<p>
-	The system will pause for about 30 seconds at the end of this
-	update.  Please <strong>do not</strong> interrupt the system
-	during this time.
-	`
-}
-data += `</font>
-</table>
-<table width="100%">
-<tr>
-<td align="left"><font size="-1" color="#D6DFD0"><small>&nbsp; &nbsp; Receiving part ${flashrom_part} of ${flashrom_total_parts}</small></font></td>
-<td align="right"><font size="-1" color="#D6DFD0"><small>v${flashrom_version} (${flashrom_type}) &nbsp; &nbsp;</small></font></td>
-</tr>
-</table>
-<center>
-<upgradeblock width=520 height=15
-${nextrompath}
-errorurl="wtv-flashrom:/lc2-download-failed"
-blockurl="wtv-flashrom:/${flashrom_rompath}
-lastblock=${flashrom_lastpart}
-curblock="${flashrom_part}"
-totalblocks="${flashrom_total_parts}"></center>
+The update is complete.<br>
+<p><font size=+1>Choose <b>Connect Now</b> if you<br>want to connect to ${services_configured.config.service_name}.
+	<p><font size=+1>Press the <b>power</b> button to switch<br>off your unit.
+</font>
 <tr>
 <td width=104 valign=middle align=center>
 <td width=20 valign=middle align=center>
-<td colspan=10 height=2 valign=middle align=center bgcolor="#191919">
+<td colspan=10 height=2 valign=middle align=center bgcolor="2B2B2B">
+<img src="wtv-flashrom:/ROMCache/Spacer.gif" width=436 height=1>
 <tr>
 <td width=104 valign=middle align=center>
 <td width=20 valign=middle align=center>
@@ -88,7 +73,8 @@ totalblocks="${flashrom_total_parts}"></center>
 <tr>
 <td width=104 valign=middle align=center>
 <td width=20 valign=middle align=center>
-<td colspan=10 height=2 valign=top align=left bgcolor="#191919">
+<td colspan=10 height=2 valign=top align=left bgcolor="0D0D0D">
+<img src="wtv-flashrom:/ROMCache/Spacer.gif" width=436 height=1>
 <tr>
 <td width=104 valign=middle align=center>
 <td width=20 valign=middle align=center>
@@ -102,9 +88,18 @@ totalblocks="${flashrom_total_parts}"></center>
 <td width=306 valign=top align=left>
 <font size="-1"><i>
 </i></font><td width=112 valign=top align=right>
-<font size="-1" color="#191919">
+<font size="-1" color="#E7CE4A">
+<shadow>
+
+<form action="client:poweroff">
+<input selected type=submit value="Connect Now" name="UseUpgradeNow"
+  borderimage="file://ROM/Borders/ButtonBorder2.bif" usestyle width=170>
+<input type="hidden" name="invalRAMImage">
+<input type="hidden" name="autoPowerOn">
+</shadow>
 </font>
 </form>
+
 </table>
 <td width=20 valign=middle align=center>
 </table>
