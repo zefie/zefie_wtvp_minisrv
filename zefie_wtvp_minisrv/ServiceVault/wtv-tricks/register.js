a37433132e337c1362f1f3ef5001070908ab3158
zefie <zefie@zefie.net>
2021-08-07 20:55:05 -0400

    wtv-tricks:/register: registering isn't dangerous! make it comfy

--- zefie_wtvp_minisrv/ServiceVault/wtv-tricks/register.js
+++ zefie_wtvp_minisrv/ServiceVault/wtv-tricks/register.js
@@ -18,20 +18,20 @@ wtv-boot-url: wtv-1800:/preregister?relogin=true`;
     message = `Are you ready to register your box with ${minisrv_config.config.service_name}?
 <br><br>
 <form action="register" method="POST" ENCTYPE="x-www-form-encoded">
-<input type="button" action="client:goback" value="Nah, I like being a Guest" borderimage="file://ROM/Borders/ButtonBorder2.bif" text="gold" >
-<input type="submit" name="confirm_register" value="Yes, I'm ready to register!" borderimage="file://ROM/Borders/ButtonBorder2.bif" text="gold" selected>
+<input type="button" action="client:goback" value="Nah, I like being a Guest" borderimage="file://ROM/Borders/ButtonBorder2.bif" text="#dadada" >
+<input type="submit" name="confirm_register" value="Yes, I'm ready to register!" borderimage="file://ROM/Borders/ButtonBorder2.bif" text="#dadada" selected>
 </form>`;
 }
 
 data = `<html>
 <head>
 <display fontsize=medium>
-<title>Danger Zone!</title>
+<title>Comfy Zone!</title>
 `;
 if (redirect) data += `<meta http-equiv=Refresh content="${redirect[0]}; url=${redirect[1]}">`;
 
 data += `</head>
-<body bgcolor="#000000" text="gold" link="gold" alink="gold" vlink="gold">
+<body bgcolor="#202020" text="#dadada" link="#dadada" alink="#dadada" vlink="#dadada">
 <br><br>
 ${message}
 </body>
