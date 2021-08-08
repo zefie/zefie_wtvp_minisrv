a91076c9b349a7a983fd41a002e9c28d1eb9359b
zefie <zefie@zefie.net>
2021-08-07 20:15:47 -0400

    implement wtv-cookie:/reset

---
+++ zefie_wtvp_minisrv/ServiceVault/wtv-cookie/reset.js
@@ -0,0 +1,25 @@
+if (socket.ssid) {
+    if (ssid_sessions[socket.ssid]) {
+        ssid_sessions[socket.ssid].resetCookies();
+        headers = "200 OK\n";
+        headers += "Content-Type: text/html";
+        data = `<html>
+<head>
+<display fontsize=medium>
+<title>Cookies cleared!</title>
+<meta http-equiv=Refresh content="3; url=client:goback?">
+</head>
+<body bgcolor="#000000" text="gold" link="gold" alink="gold" vlink="gold">
+<br><br>
+Your cookies have successfully been cleared!<br>
+Redirecting shortly... <a href="client:goback">Go Back</a>
+</body>
+</html>`;
+    }
+}
+
+if (!headers) {
+    var errpage = doErrorPage(400)
+    headers = errpage[0];
+    data = errpage[1];
+}
