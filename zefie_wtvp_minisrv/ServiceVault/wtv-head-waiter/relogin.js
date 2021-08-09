7a09c5f3b12eb39ba6343e05512a7c12d6543836
zefie <zefie@zefie.net>
2021-08-08 20:06:03 -0400

    use wtv-head-waiter:/relogin for boot url - viewer seems to retain only wtv-* and wtv-head-waiter, so lets try to be closer to protocol and boot with a wtv-head-waiter address instead of wtv-1800 - we still handle via wtv-1800 but we accept wtv-head-waiter:/relogin and send the client on its way to the relogin path

---
+++ zefie_wtvp_minisrv/ServiceVault/wtv-head-waiter/relogin.js
@@ -0,0 +1,20 @@
+var gourl = "wtv-1800:/preregister?";
+if (request_headers.query.relogin) gourl += "relogin=true";
+else if (request_headers.query.reconnect) gourl += "reconnect=true";
+
+if (request_headers.query.guest_login) {
+	if (request_headers.query.relogin || request_headers.query.reconnect) gourl += "&";
+	gourl += "guest_login=true";
+	if (request_headers.query.skip_splash) gourl += "&skip_splash=true";
+}
+
+headers = `200 OK
+Connection: Keep-Alive
+Expires: Wed, 09 Oct 1991 22:00:00 GMT
+wtv-expire-all: wtv-head-waiter:
+wtv-expire-all: wtv-1800:
+wtv-service: reset
+${getServiceString('wtv-1800')}
+wtv-visit: ${gourl}
+Content-type: text/html`;
+data = '';
