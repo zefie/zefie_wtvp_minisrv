9b25b0b86016ce599173d720c082b63f12775285
zefie <zefie@zefie.net>
2021-08-10 22:54:25 -0400

    update userstore to allow path= too

--- zefie_wtvp_minisrv/ServiceVault/wtv-disk/userstore.js
+++ zefie_wtvp_minisrv/ServiceVault/wtv-disk/userstore.js
@@ -1,9 +1,9 @@
 if (request_headers.post_data) {
-    if (request_headers.query.partialPath) {
+    if (request_headers.query.partialPath || request_headers.query.path) {
         if (socket.ssid) {
             if (ssid_sessions[socket.ssid]) {
                 if (ssid_sessions[socket.ssid].isRegistered()) {
-                    var result = ssid_sessions[socket.ssid].storeUserStoreFile(request_headers.query.partialPath, new Buffer.from(request_headers.post_data.toString(CryptoJS.enc.Hex), 'hex'), request_headers.query['last-modified-seconds'] || null, (request_headers.query.no_overwrite) ? false : true);
+                    var result = ssid_sessions[socket.ssid].storeUserStoreFile(request_headers.query.path || request_headers.query.partialPath, new Buffer.from(request_headers.post_data.toString(CryptoJS.enc.Hex), 'hex'), request_headers.query['last-modified-seconds'] || null, (request_headers.query.no_overwrite) ? false : true);
                     if (result) {
                         headers = "200 OK\n";
                         headers += "Content-Type: text/plain";
