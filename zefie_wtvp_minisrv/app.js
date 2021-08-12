5b0b25bca69188ce9669999a973fbf7f8e1aab17
zefie <zefie@zefie.net>
2021-08-12 17:58:10 -0400

    apply system-wide dont_compress query to only wtv downloads

--- zefie_wtvp_minisrv/app.js
+++ zefie_wtvp_minisrv/app.js
@@ -610,8 +610,10 @@ async function sendToClient(socket, headers_obj, data) {
     var compression_type = 0;
     if (content_length >= 256) compression_type = wtvmime.shouldWeCompress(ssid_sessions[socket.ssid], headers_obj);
     if (socket_sessions[socket.id].request_headers.query) {
-        if (socket_sessions[socket.id].request_headers.query.dont_compress) {
-            compression_type = 0;
+        if (socket_sessions[socket.id].wtv_request_type == "download") {
+            if (socket_sessions[socket.id].request_headers.query.dont_compress) {
+                compression_type = 0;
+            }
         }
     }
 
