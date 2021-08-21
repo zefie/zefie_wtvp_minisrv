863d57467eb439fafaab52430f857095b76e6d8d
zefie <zefie@zefie.net>
2021-08-21 12:27:01 -0400

    also fix lc2-download-complete (I'm having deja vu)

--- zefie_wtvp_minisrv/ServiceVault/wtv-flashrom/lc2-download-complete.js
+++ zefie_wtvp_minisrv/ServiceVault/wtv-flashrom/lc2-download-complete.js
@@ -1,3 +1,5 @@
+var minisrv_service_file = true;
+
 headers = `200 OK
 Connection: Close
 wtv-connection-close: true
@@ -42,8 +44,7 @@ Updating complete
 <td colspan=12 width=560 height=10 valign=top align=left>
 <img src="${service_name}:/ROMCache/S40H1.gif" width=560 height=6>
 <tr>
-<td width=104 height=10 valign=top align=left>var minisrv_service_file = true;
-
+<td width=104 height=10 valign=top align=left>
 
 <td width=20 valign=top align=left>
 <td width=67 valign=top align=left>
