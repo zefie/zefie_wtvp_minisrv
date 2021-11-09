f0db3813373a5df39391ebe191bea7955de4b31a
zefie <zefie@zefie.net>
2021-11-09 11:56:56 -0500

    show ScreenBorder option to Old Classic units since it seems to work

--- zefie_wtvp_minisrv/ServiceVault/wtv-setup/validate-screen-border.js
+++ zefie_wtvp_minisrv/ServiceVault/wtv-setup/validate-screen-border.js
@@ -2,7 +2,7 @@ if (request_headers.query) {
     if (request_headers.query.shade) {
         headers = `300 OK
 Location: wtv-setup:/screen
-wtv-visit: client:setscreenborder?${request_headers.query.shade}`;
+wtv-visit: client:setscreenborder?shade=${request_headers.query.shade}`;
     } else {
         var errpage = wtvshared.doErrorPage(400);
         headers = errpage[0];
