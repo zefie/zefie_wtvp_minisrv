42d6ff4181189378b1c656ecbd37dfa1621ef892
zefie <zefie@zefie.net>
2021-11-09 11:42:03 -0500

    update WTVBGMusic class/system - rename getCategoryList to getCategorySongList - add proper getCategoryList which filters client usable categories - do not show empty categories to classic clients

--- zefie_wtvp_minisrv/ServiceVault/wtv-setup/set-bg.js
+++ zefie_wtvp_minisrv/ServiceVault/wtv-setup/set-bg.js
@@ -3,7 +3,7 @@ var minisrv_service_file = true;
 var WTVBGMusic = require("./WTVBGMusic.js");
 var wtvbgm = new WTVBGMusic(minisrv_config, ssid_sessions[socket.ssid]);
 if (request_headers.query.category) {
-	var musicList = wtvbgm.getCategoryList(request_headers.query.category);
+	var musicList = wtvbgm.getCategorySongList(request_headers.query.category);
 	var categoryName = wtvbgm.getCategoryName(request_headers.query.category);
 
 	headers = `200 OK
