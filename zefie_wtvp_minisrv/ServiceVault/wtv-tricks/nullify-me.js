cf3e15412e916b4742027eab10aaef2df7760f81
zefie <zefie@zefie.net>
2022-02-15 18:25:17 -0500

    Using an invalid user_id is the better way to force null bug

--- zefie_wtvp_minisrv/ServiceVault/wtv-tricks/nullify-me.js
+++ zefie_wtvp_minisrv/ServiceVault/wtv-tricks/nullify-me.js
@@ -1,11 +1,13 @@
 var minisrv_service_file = true;
 
+// null is the new demo/flash
+
 if (ssid_sessions[socket.ssid]) {
     if (ssid_sessions[socket.ssid].data_store.wtvsec_login) {
         if (ssid_sessions[socket.ssid].data_store.wtvsec_login.ticket_store) {
             if (ssid_sessions[socket.ssid].data_store.wtvsec_login.ticket_store.user_id != null) {
                 if (ssid_sessions[socket.ssid].data_store.wtvsec_login.ticket_store.user_id >= 0)
-                    ssid_sessions[socket.ssid].switchUserID(null);
+                    ssid_sessions[socket.ssid].switchUserID(-1);
             }
         }
     }
