bec78d01f9d1ed68ccc9f08aec23e49c2c01b6d9
zefie <zefie@zefie.net>
2022-10-08 11:30:47 -0400

    add at least some sanity checking to verify-address

--- zefie_wtvp_minisrv/ServiceVault/wtv-mail/verify-address.js
+++ zefie_wtvp_minisrv/ServiceVault/wtv-mail/verify-address.js
@@ -5,9 +5,18 @@ headers = `200 OK
 Content-type: text/plain
 minisrv-no-mail-count: true`
 
-var fail_response = '0';
-var success_response = '1';
-
+var fail = '0';
+var ok = '1';
 
 // TODO: logic to check if account exists
-data = success_response;
+// All this does is some sanity checks for now
+// but does not verify the account exists
+
+if (request_headers.query.address) {
+    var address_split = request_headers.query.address.split("@");
+    var domain = address_split[1];
+    if (domain != "escargot.chat" && domain != "escargot.chat") data = fail
+    else data = ok;
+} else {
+    data = fail;
+}
