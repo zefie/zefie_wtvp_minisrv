60cdb3bb727e3d8b225bee61f10537a90ae01049
zefie <zefie@zefie.net>
2022-10-16 12:30:46 -0400

    fix FastSplash setting not saving to profile

--- zefie_wtvp_minisrv/ServiceVault/wtv-setup/validate-tweaks.js
+++ zefie_wtvp_minisrv/ServiceVault/wtv-setup/validate-tweaks.js
@@ -2,9 +2,8 @@ var minisrv_service_file = true;
 
 
 var fast_splash = wtvshared.parseBool(request_headers.query.fast_splash);
-
-
 session_data.set("fast_splash", fast_splash);
+session_data.saveSessionData();
 
 headers = `200 OK
 Connection: Keep-Alive
