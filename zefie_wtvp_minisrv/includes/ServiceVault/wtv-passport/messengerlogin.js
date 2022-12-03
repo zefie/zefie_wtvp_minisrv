d70e3462e6ea7b69914457af53a30254330071e7
zefie <zefie@zefie.net>
2022-12-02 20:57:40 -0500

    fix messengerlogin for accounts that turn on messenger but didn't fill out the required information

--- zefie_wtvp_minisrv/includes/ServiceVault/wtv-passport/messengerlogin.js
+++ zefie_wtvp_minisrv/includes/ServiceVault/wtv-passport/messengerlogin.js
@@ -4,52 +4,56 @@ wtv-encrypted: true
 Expires: Wed, 09 Oct 1991 22:00:00 GMT
 Content-Type: text/plain`
 
+var messenger_email = session_data.getSessionData("messenger_email");;
+var messenger_password = session_data.getSessionData("messenger_password");
 
-var email = session_data.getSessionData("messenger_email") + "%40" + session_data.getSessionData("messenger_domain");
-var password = session_data.decryptPassword(session_data.getSessionData("messenger_password"));
-var challenge = request_headers.request.split('?')[1];
+if (messenger_email && messenger_password) {
+	var email = messenger_email + "%40" + session_data.getSessionData("messenger_domain");
+	var password = session_data.decryptPassword(messenger_password);
+	var challenge = request_headers.request.split('?')[1];
 
-if (request_headers.request.split('?')[1].substring(0, 3) != "ct=") {
-	console.log(" *** Logging into Messenger via MSNP3")
-	data = crypto.createHash('md5').update(request_headers.request.split('?')[1] + password).digest("hex");
-} else {
-	console.log(" *** Logging into Messenger via MSNP8")
-	request_is_async = true; // Make us async
-	const request = https.get('https://msnmsgr.escargot.chat/rdr/pprdr.asp', (response) => {
-		let req_data = '';
-		response.on('data', (chunk) => {
-			req_data += chunk.toString();
-		});
-
-		response.on('end', () => {
-			var passporturls = response.headers['passporturls'].split("DALogin=")[1];
-			request.end();
-			var options = {
-				method: 'GET',
-				headers: { "Authorization": "Passport1.4 OrgVerb=GET,OrgURL=http%3A%2F%2Fmessenger%2Emsn%2Ecom,sign-in=" + email + ",pwd=" + encodeURIComponent(password) + "," + challenge }
-			}
-			const request2 = https.get(passporturls, options, (response) => {
-				let req_data = '';
-				response.on('data', (chunk) => {
-					req_data += chunk.toString();
-				});
-
-				response.on('end', () => {
-					var pp = response.headers['authentication-info'];
-					pp = pp.split("from-PP='")[1];
-					pp = pp.split("'")[0];
-					data = pp;
-					sendToClient(socket, headers, data);
-				});
-			});
-			request2.on('error', (error) => {
-				console.log(' *** Error (Stage 1)', error);
+	if (request_headers.request.split('?')[1].substring(0, 3) != "ct=") {
+		console.log(" *** Logging into Messenger via MSNP3")
+		data = crypto.createHash('md5').update(request_headers.request.split('?')[1] + password).digest("hex");
+	} else {
+		console.log(" *** Logging into Messenger via MSNP8")
+		request_is_async = true; // Make us async
+		const request = https.get('https://msnmsgr.escargot.chat/rdr/pprdr.asp', (response) => {
+			let req_data = '';
+			response.on('data', (chunk) => {
+				req_data += chunk.toString();
 			});
 
-		});
-	});
+			response.on('end', () => {
+				var passporturls = response.headers['passporturls'].split("DALogin=")[1];
+				request.end();
+				var options = {
+					method: 'GET',
+					headers: { "Authorization": "Passport1.4 OrgVerb=GET,OrgURL=http%3A%2F%2Fmessenger%2Emsn%2Ecom,sign-in=" + email + ",pwd=" + encodeURIComponent(password) + "," + challenge }
+				}
+				const request2 = https.get(passporturls, options, (response) => {
+					let req_data = '';
+					response.on('data', (chunk) => {
+						req_data += chunk.toString();
+					});
 
-	request.on('error', (error) => {
-		console.log(' *** Error (Stage 1)', error);
-	});
+					response.on('end', () => {
+						var pp = response.headers['authentication-info'];
+						pp = pp.split("from-PP='")[1];
+						pp = pp.split("'")[0];
+						data = pp;
+						sendToClient(socket, headers, data);
+					});
+				});
+				request2.on('error', (error) => {
+					console.error(' *** Error (Stage 1)', error);
+				});
+
+			});
+		});
+
+		request.on('error', (error) => {
+			console.error(' *** Error (Stage 1)', error);
+		});
+	}
 }
