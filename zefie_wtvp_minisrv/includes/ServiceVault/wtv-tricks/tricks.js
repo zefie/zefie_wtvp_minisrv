9a2358ae5218aa4d8a793c07ddd8ed4bdd64787d
zefie <zefie@zefie.net>
2024-12-19 11:03:01 -0500

    tweak some wtv-tricks html

--- zefie_wtvp_minisrv/includes/ServiceVault/wtv-tricks/tricks.js
+++ zefie_wtvp_minisrv/includes/ServiceVault/wtv-tricks/tricks.js
@@ -21,14 +21,7 @@ tricks = [
 	["client:ResetNVAndPowerOff", "Blast NVRAM"],
 	["wtv-tricks:/charmap", "Character Map"],
 	["wtv-tricks:/cSetup", "Connect Setup"],
-	["wtv-tricks:/benchmark", "Speed Test"],
-	["", ""],
-	["", ""],
-	["", ""],
-	["", ""],
-	["", ""],
-	["", ""],
-	["", ""],
+	["wtv-tricks:/benchmark", "Speed Test"]
 ]
 // add these at the bottom
 tricks.push((session_data.getSessionData("registered")) ? ["wtv-tricks:/unregister", "Unregister This Box"] : ["wtv-tricks:/register", "Register This Box"]); // reg/unreg
@@ -39,24 +32,23 @@ data = `<html>
 <script src=/ROMCache/h.js></script><script src=/ROMCache/n.js></script><script>
 head('${minisrv_config.config.service_name} Tricks')</script>
 <table cellspacing=0 cellpadding=0><tr><td abswidth=10>&nbsp;<td colspan=3>
-<br>
-<table>`;
+<table><tc><td width=50>&nbsp;</td></tc><tc><td><table>`;
 
 for (i = 0; i < tricks.length; i += 2) {
 	data += `<tr>
 <td colspan=3 height=6>
 <tr>
-<td>${(tricks[i][0] != "") ? `<a href="${tricks[i][0]}">${tricks[i][1]}</a>` : `<!-- TODO --> &nbsp;`}
+<td>${(tricks[i][0] != "") ? `&#128; <a href="${tricks[i][0]}">${tricks[i][1]}</a>` : `<!-- TODO --> &nbsp;`}
 <td width=25>
 <td>`
 	if (i + 1 < tricks.length) {
-		data += (tricks[i + 1][0] != "") ? `<a href="${tricks[i + 1][0]}">${tricks[i + 1][1]}</a>` : `<!-- TODO --> &nbsp;`
+		data += (tricks[i + 1][0] != "") ? `&#128; <a href="${tricks[i + 1][0]}">${tricks[i + 1][1]}</a>` : `<!-- TODO --> &nbsp;`
 	} else {
 		// require even number of tricks
 		data += "<!-- TODO --> &nbsp;"
 	}
 }
-data += `</table>
+data += `</table></td></tc></table>
 </body>
 </html>
 `;
