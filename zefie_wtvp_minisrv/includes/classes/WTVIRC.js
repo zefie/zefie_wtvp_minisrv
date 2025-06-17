c81795a4c5946210c939f0063b7e0bb230a89378
zefie <zefie@zefie.net>
2025-06-17 17:57:41 -0400

    fix typo

--- zefie_wtvp_minisrv/includes/classes/WTVIRC.js
+++ zefie_wtvp_minisrv/includes/classes/WTVIRC.js
@@ -4321,7 +4321,7 @@ class WTVIRC {
         for (const [index, socket] of this.clients.entries()) {
             if (socket !== clientSocket && this.isSpyingOnConnections(socket.nickname)) {
                 if (quitMsg) {
-                    this.sendWebTVNoticeTo(socket, `*** Notice --- Client exiting: ${clientSocket.nickname} (${clientSocket.username}!@${clientSocket.host}) [${clientSocket.realhost} [${quitMsg}]`);
+                    this.sendWebTVNoticeTo(socket, `*** Notice --- Client exiting: ${clientSocket.nickname} (${clientSocket.username}!@${clientSocket.host}) [${clientSocket.realhost}] [${quitMsg}]`);
                 } else {
                     this.sendWebTVNoticeTo(socket, `*** Notice --- Client connecting: ${clientSocket.nickname} (${clientSocket.username}!@${clientSocket.host}) [${clientSocket.realhost} {users} [${clientSocket.userinfo}] <${clientSocket.uniqueId}>`);
                 }
