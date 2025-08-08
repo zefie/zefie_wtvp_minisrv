932593aa21adc6afa30bd04de87f6aba6ed4f798
zefie <zefie@zefie.net>
2025-08-07 20:47:59 -0400

    poweredby url

--- zefie_wtvp_minisrv/includes/ServiceVault/wtv-flashrom/lc2-download-complete.js
+++ zefie_wtvp_minisrv/includes/ServiceVault/wtv-flashrom/lc2-download-complete.js
@@ -2,7 +2,6 @@ var minisrv_service_file = true;
 
 headers = `200 OK
 Content-type: text/html
-wtv-expire-all: wtv-flashrom:
 minisrv-no-mail-count: true`
 
 if (socket.ssid && ssid_sessions[socket.ssid]) {
