f2e117140fee2503432f2d4d1cc94ffe1d6e36d1
zefie <zefie@zefie.net>
2026-04-23 16:42:34 -0400

    version update, also make shenanigans a bitwise value

--- zefie_wtvp_minisrv/includes/classes/WTVShenanigans.js
+++ zefie_wtvp_minisrv/includes/classes/WTVShenanigans.js
@@ -1,12 +1,12 @@
 class WTVShenanigans {
     minisrv_config = null;
     shenanigans = {
-        // PLEASE NOTE: anything that is broken with any shenanigan level besides "false" is NOT a bug!!!! 
+        // PLEASE NOTE: anything that is broken with any shenanigan level besides "false/0" is NOT a bug!!!! 
 
-        "NO_SHENANIGANS": false, // no shenanigans, minisrv as intended, most secure option
+        "NO_SHENANIGANS": 0, // no shenanigans, minisrv as intended, most secure option
         "ENABLE_TRICKS_URLACCESS": 1, // allows users to use wtv-tricks:/access?url=
-        "DISABLE_HTML_ENTITIZER": 4, // disables HTML Entitizer, allowing things such as HTML in email/usenet subjects
-        "DISABLE_HTML_SANITIZER": 5 // disables HTML Sanitizer, allowing all sorts of chaos in email/usenet posts and signatures
+        "DISABLE_HTML_ENTITIZER": 2, // disables HTML Entitizer, allowing things such as HTML in email/usenet subjects
+        "DISABLE_HTML_SANITIZER": 4 // disables HTML Sanitizer, allowing all sorts of chaos in email/usenet posts and signatures
     }
 
     /**
@@ -36,20 +36,8 @@ class WTVShenanigans {
         // shenanigans are disabled, don't iterate
         if (level === false) return false;
 
-        let retval = false;
-        const shenanigans = this.shenanigans;
-
-        // shenanigans are enabled, so check if the requested shenanigan is within the level enabled
-        Object.keys(shenanigans).forEach((k) => {
-            if (shenanigans[k] === value) {
-                if (level >= shenanigans[k]) {
-                    retval = true;
-                    return false;
-                }
-            }
-        });
-
-        return retval;
+        // shenanigans are enabled, so check if the requested shenanigan is enabled using bitwise AND
+        return (level & value) === value;
     }
 }
 module.exports = WTVShenanigans;
